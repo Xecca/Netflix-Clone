527bf8bf19f155a3dab898649692a43eeb929485
Andrew Po <ensimgen@gmail.com>
2022-02-19 20:28:13 +0300

    Added .gitignore file.

---
+++ Netflix Clone/Controllers/UpcomingViewController.swift
@@ -0,0 +1,19 @@
+//
+//  UpcomingViewController.swift
+//  Netflix Clone
+//
+//  Created by Dreik on 2/19/22.
+//
+
+import UIKit
+
+class UpcomingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBackground
+    }
+    
+
+}
