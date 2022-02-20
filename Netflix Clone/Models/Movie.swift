8fa05be36e4c0c9b7e1f74fb0e40f05aaaeaf56a
Andrew Po <ensimgen@gmail.com>
2022-02-20 19:46:47 +0300

    Sending URL Requests and Parsing JSON response.

---
+++ Netflix Clone/Models/Movie.swift
@@ -0,0 +1,24 @@
+//
+//  Movie.swift
+//  Netflix Clone
+//
+//  Created by Dreik on 2/20/22.
+//
+
+import Foundation
+
+struct TrendingMoviesResponse: Codable {
+    let results: [Movie]
+}
+
+struct Movie: Codable {
+    var id: Int
+    let media_type: String?
+    let original_language: String?
+    let original_title: String?
+    let poster_path: String?
+    let release_date: String?
+    let overview: String?
+    let vote_average: Double
+    let vote_count: Int
+}
