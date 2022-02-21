e9611826b8edfc396e5552ca97ff1955b14e6ab2
Andrew Po <ensimgen@gmail.com>
2022-02-21 20:06:14 +0300

    Created custom TableViewCell from the upcoming table.

---
+++ Netflix Clone/ViewModels/TitleViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  TitleViewModel.swift
+//  Netflix Clone
+//
+//  Created by Dreik on 2/21/22.
+//
+
+import Foundation
+
+struct TitleViewModel {
+    let titleName: String
+    let posterURL: String
+}
