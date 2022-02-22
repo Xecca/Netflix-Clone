b6ac1b432bcaffb78efb485fecd7a41efbb29884
Andrew Po <ensimgen@gmail.com>
2022-02-22 20:53:39 +0300

    Created SearchResultsViewController to display search results.

---
+++ Netflix Clone/Controllers/General/SearchResultsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SearchResultsViewController.swift
+//  Netflix Clone
+//
+//  Created by Dreik on 2/22/22.
+//
+
+import UIKit
+
+class SearchResultsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemGreen
+    }
+
+}
