420516e50774cab3f2b13dcca46ab82858c6cfd4
Andrew Po <ensimgen@gmail.com>
2022-02-22 22:34:08 +0300

    Created TitlePreviewViewController.

---
+++ Netflix Clone/ViewModels/TitlePreviewViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  TitlePreviewViewModel.swift
+//  Netflix Clone
+//
+//  Created by Dreik on 2/22/22.
+//
+
+import Foundation
+
+struct TitlePreviewViewModel {
+    let title: String
+    let youtubeView: VideoElement
+    let titleOverview: String
+}
