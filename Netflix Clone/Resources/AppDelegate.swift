cec65101f45b073a664b998b51ed4e14a1782cac
Andrew Po <ensimgen@gmail.com>
2022-02-23 14:41:50 +0300

    Added new Entite with attributes.

--- Netflix Clone/Resources/AppDelegate.swift
+++ Netflix Clone/Resources/AppDelegate.swift
@@ -42,7 +42,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
          application to it. This property is optional since there are legitimate
          error conditions that could cause the creation of the store to fail.
         */
-        let container = NSPersistentContainer(name: "TestProject")
+        let container = NSPersistentContainer(name: "NetflixCloneModel")
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
             if let error = error as NSError? {
                 // Replace this implementation with code to handle the error appropriately.
@@ -64,7 +64,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     // MARK: - Core Data Saving support
 
-    func saveContext () {
+    func saveContext () {   // context manager
         let context = persistentContainer.viewContext
         if context.hasChanges {
             do {
